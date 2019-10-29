6b0537dda3d1ba6738ebb071ba08fa0e55173aea
Alay Shah <alay.shah@nutanix.com>
2019-10-28 17:53:54 -0700

    Fix name
    w was missing wire :-)

--- prep_remote.sh
+++ prep_remote.sh
@@ -4,6 +4,6 @@ scp get-pipe.py \
     setmeup.sh \
     v1.1.0.tar.gz \
     snooper.tar.gz \
-    ireshark-2.6.3-1.x86_64.rpm \
+    wireshark-2.6.3-1.x86_64.rpm \
     wireshark-qt-2.6.3-1.x86_64.rpm \
     root@$1:~/
