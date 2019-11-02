65a10d60c504ba454ebb3a47ede93554cb4bed0d
Alay Shah <alay.shah@nutanix.com>
2019-11-01 17:45:34 -0700

    File structure change

--- setmeup.sh
+++ setmeup.sh
@@ -1,16 +1,20 @@
 set -ex
+tar zxvf snooper.tar.gz
+pushd snooper
 yum install -y epel-release
-yum install -y wireshark-2.6.3-1.x86_64.rpm
-yum install -y wireshark-qt-2.6.3-1.x86_64.rpm
+yum install -y packages/wireshark-2.6.3-1.x86_64.rpm
+yum install -y packages/wireshark-qt-2.6.3-1.x86_64.rpm
 yum install -y python-devel
 
-python get-pipe.py
+python packages/get-pipe.py
 pip install virtualenv
-tar zxvf snooper.tar.gz
 virtualenv -p python2.7 env
 source env/bin/activate
 
-tar zxvf v1.1.0.tar.gz
+_TPM="env/.nutanix.tmp"
+mkdir -p ${_TPM}
+tar zxvf packages/v1.1.0.tar.gz -C ${_TPM}
+pushd ${_TPM}
 pushd basemap-1.1.0/geos-3.3.3
 ./configure --enable-python
 make
@@ -21,6 +25,7 @@ popd
 pushd  basemap-1.1.0
 python setup.py install
 popd
+popd
 
-pip install -r requirements.txt
-rm -rf basemap-1.1.0/ v1.1.0.tar.gz  wireshark-*
+pip install -r etc/requirements.txt
+rm -rf ${_TPM}
