596d7576fbfa51e43386949d194bc7b191647010
Alay Shah <alay.shah@nutanix.com>
2019-11-06 15:43:19 -0800

    Adding packages

--- setmeup.sh
+++ setmeup.sh
@@ -1,4 +1,10 @@
 set -ex
+
+_orig_cwd="$PWD"
+pipinstall() {
+  echo "$@"
+  pip install --no-index --find-links ${_orig_cwd}/snooper/packages/PyPackages "$@"
+}
 tar zxvf snooper.tar.gz
 pushd snooper
 yum install -y epel-release
@@ -6,8 +12,8 @@ yum install -y packages/wireshark-2.6.3-1.x86_64.rpm
 yum install -y packages/wireshark-qt-2.6.3-1.x86_64.rpm
 yum install -y python-devel
 
-python packages/get-pipe.py
-pip install virtualenv
+python packages/get-pipe.py --no-index --find-links=packages/PyPackages/
+pipinstall virtualenv
 virtualenv -p python2.7 env
 source env/bin/activate
 
@@ -20,12 +26,12 @@ pushd basemap-1.1.0/geos-3.3.3
 make
 make install
 
-pip install matplotlib
+pipinstall matplotlib
 popd
 pushd  basemap-1.1.0
 python setup.py install
 popd
 popd
 
-pip install -r etc/requirements.txt
+pipinstall -r etc/requirements.txt
 rm -rf ${_TPM}
